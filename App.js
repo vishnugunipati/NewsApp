700b609d2e174f7aaebd80b639402925a48299b4
vishnugunipati <vishnugunipati@gmail.com>
2020-01-06 11:27:41 +0530

    added news app

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import TabScreen from './src/screens/TabScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <TabScreen/>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ src/config/rest_consfig.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'in';
+export const category = 'general';
+export const _api_key = '0e59b6ff189347d991a06a408bb9b4fa';
